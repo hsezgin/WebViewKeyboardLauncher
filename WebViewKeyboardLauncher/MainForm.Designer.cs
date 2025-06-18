9861e326bfa33d5d08db5eb8d69676cae96ab5fe
hsezgin <h.sezgin.b@gmail.com>
2025-06-19 00:37:31 +0300

    Apache 2.0 lisans

--- WebViewKeyboardLauncher/MainForm.Designer.cs
+++ WebViewKeyboardLauncher/MainForm.Designer.cs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 SezginBilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 namespace WebViewKeyboardLauncher
 {
     partial class MainForm
@@ -5,10 +21,10 @@ namespace WebViewKeyboardLauncher
         private void InitializeComponent()
         {
             this.SuspendLayout();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None; // Scaling problemi önlemek için
-            this.ClientSize = new System.Drawing.Size(800, 450); // Bu deðer MainForm.cs'de ezilecek
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None; // Scaling problemi ï¿½nlemek iï¿½in
+            this.ClientSize = new System.Drawing.Size(800, 450); // Bu deï¿½er MainForm.cs'de ezilecek
             this.Name = "MainForm";
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // Tam ekran için
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // Tam ekran iï¿½in
             this.WindowState = System.Windows.Forms.FormWindowState.Normal; // Normal state
             this.ResumeLayout(false);
         }
